8fd330e147ded81dd31d3a7b179beed27a775ece
ottojonas <ottobjonas@outlook.com>
2025-01-20 15:03:29 +0000

    refresh for demo purposes

--- backend/app/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify, request 
-
-routes = Blueprint("routes", __name__)
-
-@routes.route('/health', methods = ["GET"])
-def health_check(): 
-    return {'status': 'healthy'}
